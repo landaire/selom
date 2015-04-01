75de5f6e9dfd5a8737501721df889d86bb126bfb
Lander Brandt <landerbrandt@gmail.com>
2015-04-01 10:22:29 -0700

    Add SSH log reading for invalid users

--- logs.go
+++ logs.go
@@ -8,20 +8,21 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"regexp"
+
+	"encoding/json"
 
 	"github.com/codegangsta/cli"
-    "encoding/json"
 )
 
 var (
-	attackers     = make(map[string]interface{})
-	attacks = make(map[string]interface{})
-	totals = make(map[string]interface{})
-	urls = make(map[string]int)
-	methods = make(map[string]int)
+	attackers           = make(map[string]interface{})
+	attacks             = make(map[string]interface{})
+	totals              = make(map[string]interface{})
+	urls                = make(map[string]int)
+	methods             = make(map[string]int)
 	interestingRequests = make(map[string]int)
-	sshLogPattern = regexp.MustCompile(`^auth.log(\.\d+)?$`)
+
+	invalidUsers = make(map[string]int)
 )
 
 func main() {
@@ -57,6 +58,7 @@ func main() {
 
 		totals["methods"] = methods
 		totals["urls"] = urls
+		totals["invalid_users"] = invalidUsers
 		attackers["attacks"] = attacks
 		attackers["totals"] = totals
 		attackers["interesting_requests"] = interestingRequests
@@ -64,7 +66,7 @@ func main() {
 		for _, fileInfo := range files {
 			// Ignore directories and files we don't care about
 			if fileInfo.IsDir() ||
-				(!NginxLogPattern.MatchString(fileInfo.Name()) && !sshLogPattern.MatchString(fileInfo.Name())) {
+				(!NginxLogPattern.MatchString(fileInfo.Name()) && !SshLogPattern.MatchString(fileInfo.Name())) {
 				continue
 			}
 
@@ -75,15 +77,17 @@ func main() {
 			}
 			defer file.Close()
 
-            if NginxLogPattern.MatchString(fileInfo.Name()) {
-			    ReadNginxLog(file)
-            }
+			if NginxLogPattern.MatchString(fileInfo.Name()) {
+				ReadNginxLog(file)
+			} else if SshLogPattern.MatchString(fileInfo.Name()) {
+				ReadSshLog(file)
+			}
 		}
 
-        encoded, _ := json.MarshalIndent(attackers, "", "    ")
+		encoded, _ := json.MarshalIndent(attackers, "", "    ")
 
-        fmt.Println(string(encoded))
+		fmt.Println(string(encoded))
 	}
 
-    app.Run(os.Args)
+	app.Run(os.Args)
 }
--- ssh.go
+++ ssh.go
@@ -1,11 +1,45 @@
 package main
+import (
+    "os"
+    "bufio"
+    "regexp"
+)
 
 const (
     SshTypePassword = "Password"
     SshType = "Key"
 )
 
+var (
+    SshLogPattern = regexp.MustCompile(`^auth.log(\.\d+)?$`)
+    failIndicator = regexp.MustCompile("(?i)fail")
+    invalidUserPattern = regexp.MustCompile(`(?i)invalid user (?P<user>\S+) from (?P<ip>[\d\.]+)`)
+)
+
 type Ssh struct {
     User string
     Type string
+}
+
+func ReadSshLog(file *os.File) {
+    scanner := bufio.NewScanner(file)
+
+    for scanner.Scan() {
+        line := scanner.Text()
+//        if !failIndicator.MatchString(line) {
+//            continue
+//        }
+
+        if invalidUserPattern.MatchString(line) {
+            match := invalidUserPattern.FindStringSubmatch(line)
+            result := make(map[string]string)
+            for i, name := range invalidUserPattern.SubexpNames() {
+                result[name] = match[i]
+            }
+
+            invalidUsers[result["user"]]++
+        } else if false {
+
+        }
+    }
 }
